9857673d4034f9cda5ae27dca80bd3c99688bf32
bokyo <yung12320@daum.net>
2019-06-24 08:36:46 +0900

    add opencv

---
+++ openalpr/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using OpenCvSharp;
+using Tesseract;
+
+namespace openalpr
+{
+    public partial class Form1 : Form
+    {
+        public static string filePath;
+        Opencv opencv;
+        Mat mat;
+
+        
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "이미지 파일(.jpg,.png)|";
+            openFileDialog1.Title = "이미지 열기";
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            string fileName = openFileDialog1.SafeFileName;
+            filePath = openFileDialog1.FileName;
+            if (openFileDialog1.FileName != "")
+            {
+                pictureBox1.Load(@filePath);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            opencv = new Opencv(filePath);
+            try
+            {
+                if (checkBox1.Checked)
+                {
+                    pictureBox1.Image = opencv.convertbinp(filePath);
+                }
+                else if((Convert.ToInt16(textBox1.Text) > 0 || Convert.ToInt16(textBox1.Text) <= 255))
+                {
+                    pictureBox1.Image = opencv.convertbinp(filePath, Convert.ToInt16(textBox1.Text));
+                }
+                else
+                {
+                    MessageBox.Show("올바른 정수값을 입력해주세요(1~255)");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("사진과 세부사항을 기재해주세요");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = !textBox1.Enabled;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {     
+            MessageBox.Show(opencv.Method1());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(opencv.Method2(new Bitmap(pictureBox1.Image)));
+        }
+    }
+}
---
+++ openalpr/Opencv.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using Tesseract;
+
+namespace openalpr
+{
+    class Opencv
+    {
+        private static readonly HttpClient client = new HttpClient();
+        Mat tmp;
+        Mat src;
+        public Opencv(string file)
+        {
+            tmp = new Mat(@file, ImreadModes.AnyColor);
+            src = new Mat(@file, ImreadModes.GrayScale);
+        }
+
+        public Bitmap convertbinp(string file, int weight)
+        {
+            Mat dst = new Mat();
+            Cv2.Threshold(src, dst, weight, 255, ThresholdTypes.Binary);
+            
+            Bitmap bitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);
+            bitmap.Save(@"D:\binaryimage.png", System.Drawing.Imaging.ImageFormat.Png);
+            return bitmap;
+        }
+        public Bitmap convertbinp(string file)
+        {
+            Mat dst = new Mat();
+            Cv2.Threshold(src, dst, 255, 255, ThresholdTypes.Binary | ThresholdTypes.Otsu);
+            
+            Bitmap bitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);
+            bitmap.Save(@"D:\binaryimage.png", System.Drawing.Imaging.ImageFormat.Png);
+            return bitmap;
+        }
+        public string Method1()
+        {
+            Task<string> recognizeTask = Task.Run(() => ProcessImage(@"D:\binaryimage.png"));
+            recognizeTask.Wait();
+            string task_result = recognizeTask.Result;
+            string result = task_result.Split(':')[11].Split(',')[0];
+            result = result.Replace("\"", "");
+            result = result.Trim();
+
+            return result;
+        }
+        public string Method2(Bitmap img)
+        {
+            var ocr = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
+            var texts = ocr.Process(img);
+            return texts.GetText();
+        }
+        public static async Task<string> ProcessImage(string image_path)
+        {
+            string SECRET_KEY = "sk_11483b352b7ac0c0a5653a4a";
+
+            Byte[] bytes = File.ReadAllBytes(image_path);
+            string imagebase64 = Convert.ToBase64String(bytes);
+
+            var content = new StringContent(imagebase64);
+
+            var response = await client.PostAsync("https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=us&secret_key=" + SECRET_KEY, content).ConfigureAwait(false);
+
+            var buffer = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+            var byteArray = buffer.ToArray();
+            var responseString = Encoding.UTF8.GetString(byteArray, 0, byteArray.Length);
+
+            return responseString;
+        }
+    }
+}
